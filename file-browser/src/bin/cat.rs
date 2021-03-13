a7b1b766b7a8b6656ec50eb8d9e8003c7f621316
huansern <aston@outlook.com>
2021-03-13 22:32:51 +0800

    chore: update cat to remove unnecessary string conversion
    Read files or stdin into byte slice and write to stdout directly. Removed unnecessary byte slice to string conversion and vice versa.

--- file-browser/src/bin/cat.rs
+++ file-browser/src/bin/cat.rs
@@ -1,27 +1,26 @@
-use std::fs;
-use std::io;
-use std::io::BufRead;
+use std::{fs, io};
+use std::io::{Read, Write};
 
-fn read_stdin() -> io::Result<()> {
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        match line {
-            Ok(input) => println!("{}", input),
-            Err(e) => {
-                return Err(e)
-            }
+fn copy(reader: &mut Box<dyn Read>, writer: &mut Box<dyn Write>, buf: &mut [u8]) -> io::Result<()> {
+    while let Ok(n) = reader.read(buf) {
+        if n == 0 {
+            break
         }
+        writer.write_all(&buf[..n])?;
+        writer.flush()?;
     }
     Ok(())
 }
 
-fn read_files(paths: Vec<String>) -> io::Result<()> {
+fn read_stdin(writer: &mut Box<dyn Write>, buf: &mut [u8]) -> io::Result<()> {
+    copy(&mut (Box::new(io::stdin()) as Box<dyn Read>), writer, buf)
+}
+
+fn read_files(paths: Vec<String>, writer: &mut Box<dyn Write>, buf: &mut [u8]) -> io::Result<()> {
     for path in paths {
-        match fs::read_to_string(path) {
-            Ok(data) => print!("{}", data),
-            Err(e) => {
-                return Err(e);
-            }
+        match fs::File::open(path) {
+            Ok(file) => copy(&mut (Box::new(file) as Box<dyn Read>), writer, buf)?,
+            Err(e) => return Err(e)
         }
     }
     Ok(())
@@ -30,10 +29,12 @@ fn read_files(paths: Vec<String>) -> io::Result<()> {
 fn main() {
     let mut args = std::env::args().collect::<Vec<String>>();
     args.remove(0);
+    let mut stdout = Box::new(io::stdout()) as Box<dyn Write>;
+    let mut buf = [0; 32 * 1024];
     let result = if args.is_empty() {
-        read_stdin()
+        read_stdin(&mut stdout, &mut buf)
     } else {
-        read_files(args)
+        read_files(args, &mut stdout, &mut buf)
     };
     match result {
         Err(e) => {
