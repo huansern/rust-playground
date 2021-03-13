f7d94e4f3d3ab112197d95ca51bd02610bb7d594
huansern <aston@outlook.com>
2021-03-13 16:23:50 +0800

    chore: update cat to accept multiple files as input

--- file-browser/src/bin/cat.rs
+++ file-browser/src/bin/cat.rs
@@ -15,20 +15,25 @@ fn read_stdin() -> io::Result<()> {
     Ok(())
 }
 
-fn read_file(path: &str) -> io::Result<()> {
-    match fs::read_to_string(path) {
-        Ok(data) => {
-            print!("{}", data);
-            Ok(())
+fn read_files(paths: Vec<String>) -> io::Result<()> {
+    for path in paths {
+        match fs::read_to_string(path) {
+            Ok(data) => print!("{}", data),
+            Err(e) => {
+                return Err(e);
+            }
         }
-        Err(e) => Err(e)
     }
+    Ok(())
 }
 
 fn main() {
-    let result = match std::env::args().nth(1) {
-        Some(input) => read_file(&input),
-        None => read_stdin()
+    let mut args = std::env::args().collect::<Vec<String>>();
+    args.remove(0);
+    let result = if args.is_empty() {
+        read_stdin()
+    } else {
+        read_files(args)
     };
     match result {
         Err(e) => {
