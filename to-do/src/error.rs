40624dd8d4b5c57919f819f7aab271606e0d5c43
huansern <aston@outlook.com>
2021-03-30 20:43:17 +0800

    chore(to-do): completing to-do app

--- to-do/src/error.rs
+++ to-do/src/error.rs
@@ -1,15 +1,44 @@
-use std::{io, result};
+use std::fmt::{Display, Formatter};
+use std::io::ErrorKind;
+use std::num::ParseIntError;
+use std::{fmt, io, num, result};
 
 pub type Result<T> = result::Result<T, Error>;
 
 pub enum Error {
     Io(io::Error),
+    ParseSequence(num::ParseIntError),
     InvalidSequence,
+    MissingDescription,
     ParseTask(String),
+}
+
+impl From<num::ParseIntError> for Error {
+    fn from(err: ParseIntError) -> Self {
+        Self::ParseSequence(err)
+    }
 }
 
 impl From<io::Error> for Error {
     fn from(err: io::Error) -> Self {
         Self::Io(err)
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::ParseSequence(_) => write!(f, "Input sequence is not a number"),
+            Error::InvalidSequence => write!(f, "Input sequence number does not exist"),
+            Error::MissingDescription => write!(f, "Missing task description"),
+            Error::ParseTask(_) => write!(f, "Error parsing to-do(s)"),
+            Error::Io(err) if err.kind() == ErrorKind::NotFound => {
+                write!(f, "Error opening file: file not found")
+            }
+            Error::Io(err) if err.kind() == ErrorKind::PermissionDenied => {
+                write!(f, "Error opening file: permission denied")
+            }
+            Error::Io(err) => write!(f, "{}", err.to_string()),
+        }
     }
 }
--- to-do/src/main.rs
+++ to-do/src/main.rs
@@ -2,6 +2,81 @@ mod app;
 mod error;
 mod task;
 
+use crate::error::{Error, Result};
+use clap::{clap_app, ArgMatches};
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let args = clap_app!(app =>
+        (name: "Quest")
+        (version: "0.1.0")
+        (about: "Quest is a simple cli to-do app.")
+        (@arg FILE: -f --file [FILE] "Specify an alternate to-do file\n(the default is quest file in current directory)")
+        (@arg EDIT: -e --edit [SEQUENCE] "Edit task description")
+        (@arg DELETE: -d --delete [SEQUENCE] "Delete a task")
+        (@arg COMPLETE: -c --complete [SEQUENCE] "Complete a task")
+        (@arg PRUNE: -p --prune "Delete completed tasks")
+        (@arg TASK: "Task description")
+    )
+        .get_matches();
+
+    let input_file = match args.value_of("FILE") {
+        None => env::current_dir().unwrap().join("quest"),
+        Some(path) => PathBuf::from(path),
+    };
+
+    let path = input_file.as_os_str().to_str().unwrap();
+
+    let r = run(&args, path);
+    print(r);
+}
+
+fn print(r: Result<String>) {
+    match r {
+        Err(err) => {
+            eprintln!("{}", err);
+            std::process::exit(1);
+        }
+        Ok(s) if s.len() > 0 => println!("{}", s),
+        Ok(_) => {}
+    }
+}
+
+fn run(args: &ArgMatches, path: &str) -> Result<String> {
+    if args.is_present("EDIT") {
+        let index = parse_sequence(args.value_of("EDIT").unwrap())?;
+        let description = get_description(args)?;
+        app::edit(path, index, &description)
+    } else if args.is_present("DELETE") {
+        let index = parse_sequence(args.value_of("DELETE").unwrap())?;
+        app::delete(path, index)
+    } else if args.is_present("COMPLETE") {
+        let index = parse_sequence(args.value_of("COMPLETE").unwrap())?;
+        app::complete(path, index)
+    } else if args.is_present("PRUNE") {
+        app::prune(path)
+    } else if args.is_present("TASK") {
+        let description = get_description(args)?;
+        app::add(path, &description)
+    } else {
+        app::print(path)
+    }
+}
+
+fn parse_sequence(s: &str) -> Result<usize> {
+    let sequence = s.parse::<usize>()?;
+    if sequence > 0 {
+        Ok(sequence - 1)
+    } else {
+        Err(Error::InvalidSequence)
+    }
+}
+
+fn get_description(args: &ArgMatches) -> Result<String> {
+    let description = args.value_of("TASK");
+    match description {
+        None => Err(Error::MissingDescription),
+        Some(desc) => Ok(desc.into()),
+    }
 }
